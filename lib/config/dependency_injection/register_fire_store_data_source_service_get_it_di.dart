574b378dd8214f855f5c7e9a5f750633e0256ca5
SuraiyaKhatunItHome <suraiya.it.home@gmail.com>
2025-05-22 23:55:26 +0600

    registerFireStoreDataSourceServiceGetItDI

---
+++ lib/config/dependency_injection/register_fire_store_data_source_service_get_it_di.dart
@@ -0,0 +1,16 @@
+import 'package:firestore_db_impl/data/data_sources/fire_store_db_crud_service_impl.dart';
+import 'package:firestore_db_impl/data/data_sources/i_data_sources/i_fire_store_db_crud_service.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:get_it_di_global_variable/get_it_di.dart';
+
+void registerFireStoreDataSourceServiceGetItDI() {
+  /// ----------------- Firebase--------------
+  // Register DatabaseReference
+  sl.registerSingleton<FirebaseDatabase>(FirebaseDatabase.instance);
+  // final FirebaseInstance firebaseService = FirebaseInstance.getInstance;
+
+  /// Service
+  sl.registerLazySingleton<IFireStoreDbCrudService>(
+        () => FireStoreDbCrudServiceImpl(),
+  );
+}
